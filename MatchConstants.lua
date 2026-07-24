ddfd6e7e679bd80eef2270f65e97afb4168a08c3
konsta-code <konstantin.lisovol@gmail.com>
2026-07-24 03:23:00 +0200

    Tempo: kürzeres Warmup + PostMatch + Overtime für hardcore feel (Phase 1)

--- MatchConstants.lua
+++ MatchConstants.lua
@@ -4,10 +4,10 @@
 local Constants = {}
 
 Constants.MIN_PLAYERS_TO_START = 1   -- für Solo-Testing; für echte Matches hochsetzen
-Constants.WARMUP_DURATION = 10       -- Sekunden Countdown vor Rundenstart
+Constants.WARMUP_DURATION = 6        -- Sekunden Countdown vor Rundenstart (vorher 10)
 Constants.MATCH_DURATION = 600       -- Sekunden (10 Minuten) Zeitlimit pro Runde
-Constants.OVERTIME_DURATION = 120   -- Sudden Death bei Gleichstand
+Constants.OVERTIME_DURATION = 90     -- Sudden Death bei Gleichstand (vorher 120)
 Constants.CAPTURES_TO_WIN = 5        -- Team gewinnt sofort bei so vielen Captures
-Constants.POSTMATCH_DURATION = 25    -- Sekunden nach Rundenende: Podium + Map-Voting
+Constants.POSTMATCH_DURATION = 18    -- Sekunden nach Rundenende: Podium + Map-Voting (vorher 25)
 
 return Constants
